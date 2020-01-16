0a76b82471d9c52421eb17487c4def47b46ccd65
NorwayWang <15822128679@163.com>
2020-01-16 22:58:32 +0800

    家中测试

--- SpringMVCMAven05-SSM02/src/main/java/com/wml/util/StringUtil.java
+++
@@ -1,9 +0,0 @@
-package com.wml;
-
-public class StringUtil {
-    /**非空判断*/
-    public static boolean isEmpty(String str){
-        boolean result =(str == null || str.length()<1)?true:false;
-        return result;
-    }
-}
--- SpringMVCMaven04-SSM01/src/main/java/com/wml/dao/UserInfoDao.java
+++
@@ -1,9 +0,0 @@
-package com.wml.dao;
-
-import com.wml.entity.UserInfo;
-import java.util.List;
-import java.util.Map;
-
-public interface UserInfoDao {
-    List<UserInfo> querryUserInfoAll();
-}
